a99673f6e342ac635aab853f3981152235eaf02d
Nicholas Hubbard <14hubbardn@gmail.com>
2016-06-08 22:32:32 -0400

    Added dependance on mongoose, they way we will interact with mongodb. wrote code to connect to database

--- clubmeets-api/app.js
+++ clubmeets-api/app.js
@@ -4,8 +4,8 @@
  */
 
 var express = require('express');
+var mongoose = require('mongoose');
 var routes = require('./routes');
-
 var app = module.exports = express.createServer();
 
 // Configuration
@@ -23,20 +23,24 @@ app.configure(function(){
     next();
   });
 });
-
+var port;
 
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  mongoose.connect('mongodb://localhost/test');
+  port = 3000;
 });
 
 app.configure('production', function(){
   app.use(express.errorHandler());
+  mongoose.connect('mongodb://107.170.40.178/');
+  port = 80;
 });
 
 // Routes
 
 app.get('/', routes.index);
 
-app.listen(3000, function(){
+app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
