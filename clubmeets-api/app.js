31f02ce25db914639a8bc174717c36f0f787c322
Nicholas Hubbard <nrhubbar@iupui.edu>
2016-08-27 14:31:57 -0400

    Trying Http://db:27017

--- clubmeets-api/app.js
+++ clubmeets-api/app.js
@@ -28,7 +28,7 @@ var port;
 
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-  mongoose.connect('mongodb://poiuytrewq:27017');
+  mongoose.connect('http://db:27017');
   port = 3000;
 });
 
