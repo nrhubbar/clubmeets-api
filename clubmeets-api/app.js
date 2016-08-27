28f49c60318fc9e8ab1bd7aa4342e360e9448e49
Nicholas Hubbard <nrhubbar@iupui.edu>
2016-08-27 14:26:33 -0400

    Fixed db connect

--- clubmeets-api/app.js
+++ clubmeets-api/app.js
@@ -28,7 +28,7 @@ var port;
 
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-  mongoose.connect('0.0.0.0:27017');
+  mongoose.connect('mongodb://db:27017');
   port = 3000;
 });
 
